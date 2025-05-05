0c1eb0b6078f2edf31ac97e662f4f70976172c59
Badshah01251 <badshahbroking@proton.me>
2025-05-05 22:39:29 +0530

    upload site

---
+++ desk-of-LateshNarula.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="NexPace">
+    <link rel="icon" type="image/x-icon" href="./Assets/SRTC_Emblem.png">
+    <title>Shri Ram Traders Club | Desk of Latesh Narula</title>
+    <link rel="stylesheet" href="./StyleSheets/GlobalStyle.css">
+    <link rel="stylesheet" href="./StyleSheets/raDeskStyle.css">
+</head>
+
+<body>
+    <!-- navigation section start -->
+    <nav>
+        <img class="logo" src="./Assets/brandIcon.svg" alt="Shri Ram Traders Club | Logo">
+        <span class="line"></span>
+        <ul class="main_nav">
+            <a href="./index.html">
+                <li>Home</li>
+            </a>
+            <a href="./about.html">
+                <li>About</li>
+            </a>
+            <a href="./services.html">
+                <li>Services</li>
+            </a>
+            <a href="./FAQs.html">
+                <li>FAQs</li>
+            </a>
+            <a href="./testimonials.html">
+                <li>Testimonials</li>
+            </a>
+            <a href="./investor-charter.html">
+                <li>Investor Charter</li>
+            </a>
+            <a href="./complaint-board.html">
+                <li>Complaint Board</li>
+            </a>
+            <a class="active">
+                <li>RA's Desk</li>
+            </a>
+            <a href="./elite-program.html">
+                <li>Elite Program</li>
+            </a>
+        </ul>
+        <button class="mobNav" id="mobNav" onclick="navToggler()">
+            <div class="navElement one"></div>
+            <div class="navElement two"></div>
+            <div class="navElement three"></div>
+        </button>
+    </nav>
+    <!-- navigation section end -->
+
+    <!-- desk section start -->
+    <div class="desk_container">
+        <div class="header_texts">
+            <p class="primary">Desk of Latesh Narula</p>
+            <p class="secondary">These given feeds are continuously updated and monitored by our SEBI Registered Research Analyst Latesh Narula under Research Firm named Latesh Narula Proprietor of Equity Dynamo having SEBI registration number <span>INH000016269</span></p>
+        </div>
+        <!-- Fetching Desk Data from Database -->
+        <div class="desk_data">
+                            <div class="content">
+
+                    <img class="post-img" src="./Admin/UploadedImage/1738347256.png" alt="!image-not-found">
+                    <div class="texts">
+                        <div class="main-texts">
+                            <p class="post_title">Nifty : Kal , Aaj aur Kal (01/02/2025) Budget Day </p>
+                            <p class="tstamp">Post Date : 2025-01-31</p>
+                            <p class="tstamp">Post Time : 08:34:31pm</p>
+                            <p style="font-family: 'Montserrat', sans-serif; text-transform: uppercase; color: white; font-size: 0.9rem; font-weight: 500; letter-spacing: 0.6px;">Analyzed on : Nifty</p>
+                        </div>
+
+                        <a href="detailed-post.php?post_id=2" class="exp_btn">Read More<ion-icon name="book"></ion-icon></a>
+                        <div class="profile">
+                            <img class="profilePhoto" src="./Assets/LNarula-icon.png" alt="!">
+                            <div class="text">
+                                <p class="name">Latesh Narula</p>
+                                <p class="desg">Research Analyst</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                            <div class="content">
+
+                    <img class="post-img" src="./Admin/UploadedImage/1738258687.jpg" alt="!image-not-found">
+                    <div class="texts">
+                        <div class="main-texts">
+                            <p class="post_title">Nifty : Kal , Aaj aur Kal  (31/01/2025)</p>
+                            <p class="tstamp">Post Date : 2025-01-30</p>
+                            <p class="tstamp">Post Time : 07:59:57pm</p>
+                            <p style="font-family: 'Montserrat', sans-serif; text-transform: uppercase; color: white; font-size: 0.9rem; font-weight: 500; letter-spacing: 0.6px;">Analyzed on : Nifty</p>
+                        </div>
+
+                        <a href="detailed-post.php?post_id=1" class="exp_btn">Read More<ion-icon name="book"></ion-icon></a>
+                        <div class="profile">
+                            <img class="profilePhoto" src="./Assets/LNarula-icon.png" alt="!">
+                            <div class="text">
+                                <p class="name">Latesh Narula</p>
+                                <p class="desg">Research Analyst</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    </div>
+    </div>
+    <!-- desk section end -->
+
+    <!-- footer section start -->
+    <footer>
+        <div class="container">
+            <div class="compDec">
+                <img class="logo" src="./Assets/brandIcon.svg" alt="Shri Ram Traders Club | Logo">
+                <p class="text">
+                    313,3rd Floor, Wonder Mall<br>
+                    Alwar (Rajasthan) - 301001
+                    <br><br>
+                    support@shriramtradersclub.com
+                </p>
+                <div class="connect">
+                    <a href="/"><ion-icon name="call"></ion-icon>+91 911-912-0995</a>
+                    <div class="rounded">
+                        <a href="https://wa.me/+919119120995"><ion-icon name="logo-whatsapp"></ion-icon></a>
+                        <a href="https://t.me/shriramtraderclub"><box-icon type='logo' name='telegram'></box-icon></a>
+                        <a href="https://www.facebook.com/share/12Agdo5kEWP/?mibextid=wwXIfr"><ion-icon
+                                name="logo-facebook"></ion-icon></a>
+                        <a href="https://www.instagram.com/shriramtradersclub1/profilecard/?igsh=a3o1NjBtZzZ3NWk1"><ion-icon
+                                name="logo-instagram"></ion-icon></a>
+                    </div>
+                </div>
+            </div>
+            <div class="links">
+                <p class="header">Legals</p>
+                <div class="link">
+                    <a href="./disclosure.html">Disclosure & Disclaimer</a>
+                    <a href="./privacy-policy.html">Privacy Policy</a>
+                    <a href="./refund-policy.html">Refund Policy</a>
+                    <a href="./grievance-redressal.html">Grievance Redressal</a>
+                </div>
+            </div>
+            <div class="links">
+                <p class="header">Useful Links</p>
+                <div class="link">
+                    <a href="./about.html">About</a>
+                    <a href="./services.html">Services</a>
+                    <a href="./FAQs.html">FAQs</a>
+                </div>
+            </div>
+        </div>
+        <div class="declaration">
+            <p>&copy; 2025 Shri Ram Traders Club | All the Rights Reserved.</p>
+            <p>Crafted by
+                <a href="https://nexpace.in/" target="_blank">
+                    <ion-icon name="open"></ion-icon>
+                    NexPace
+                </a>
+            </p>
+        </div>
+    </footer>
+    <!-- footer section end -->
+
+
+
+    <!-- Icon Package Integration -->
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <!-- Navigation JS -->
+    <script>
+        const section = document.querySelectorAll('.section');
+        const navLinks = document.querySelectorAll('nav ul a');
+        window.onscroll = () => {
+            section.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop - 77;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+                if (top >= offset && top < offset + height) {
+                    navLinks.forEach(links => {
+                        links.classList.remove('active');
+                        document.querySelector('nav ul a[href*=' + id + ']').classList.add('active');
+                    });
+                };
+            });
+        };
+    </script>
+    <!-- MobNav JS -->
+    <script>
+        let nav = document.querySelector('.main_nav');
+        let mobNav = document.getElementById('mobNav');
+
+        function navToggler() {
+            nav.classList.toggle('show-nav');
+            mobNav.classList.toggle('mobNavCng');
+        }
+
+        function closeNav() {
+            nav.classList.remove('show-nav');
+            mobNav.classList.remove('mobNavCng');
+        }
+    </script>
+</body>
+
+</html>
